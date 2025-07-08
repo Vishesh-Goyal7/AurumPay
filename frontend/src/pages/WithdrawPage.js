650b6118596cd27da588e301edd7ba903c3400d3
Vishesh <123769123+Vishesh-Goyal7@users.noreply.github.com>
2025-07-08 23:40:23 +0530

    Update WithdrawPage.js

--- frontend/src/pages/WithdrawPage.js
+++ frontend/src/pages/WithdrawPage.js
@@ -19,7 +19,7 @@ function WithdrawPage() {
     }
 
     try {
-      const res = await fetch('https://aurumpay.visheshverse.com/api/wallet/withdraw', {
+      const res = await fetch('https://aurumpay.onrender.com/api/wallet/withdraw', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
